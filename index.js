dbffcbbb9554d28028ee8f66f6fe41deb46e4bf9
Dobrin Dimchev <dobrin.dimchev@sap.com>
2021-10-11 21:23:53 +0300

    add target repo

--- index.js
+++ index.js
@@ -1,44 +1,74 @@
-const fs = require("fs");
-const path = require("path");
 const axios = require("axios");
 
 require("dotenv").config();
 
 const headersList = {
   Accept: "application/vnd.github.cloak-preview",
+  'Authorization': `token ${token}`
 };
 
-// const delimiter = ";;";
 const github = process.env.GH_URL;
 const org = process.env.GH_ORG;
 const repo = process.env.GH_REPO;
-const outDir = path.join(__dirname, "out");
 
-if (!fs.existsSync(outDir)) {
-  fs.mkdirSync(outDir);
-}
+const githubTo = process.env.GH_URL_TO;
+const orgTo = process.env.GH_ORG_TO;
+const repoTo = process.env.GH_REPO_TO;
+const token = process.env.TOKEN;
 
-const doRequest = async () => {
+const createIssue = async (data) => {
+  console.log("Creating issue:", data);
+
   const reqOptionsIssues = {
-    url: `${github}/repos/${org}/${repo}/issues`,
-    method: "GET",
-    headers: headersList,
-    auth: {
-      username: process.env.GH_USER,
-      password: process.env.GH_TOKEN,
+    url: `${githubTo}/repos/${orgTo}/${repoTo}/issues`,
+    method: "POST",
+    headers: {
+      ...headersList,
+      'Content-Type': 'application/json',
     },
+    data,
   };
 
   const response = await axios
     .request(reqOptionsIssues)
     .catch((error) => console.log(error));
 
-  // do something with response.data
-  console.log(response.data[0]);
+  console.log(response?.status, response?.statusText);
+
+  return {
+    success: response?.status >= 200 && response?.status < 300
+  }
+}
+
+const getIssues = async () => {
+  const reqOptionsIssues = {
+    url: `${github}/repos/${org}/${repo}/issues`,
+    method: "GET",
+    headers: headersList,
+  };
+
+  const response = await axios
+    .request(reqOptionsIssues)
+    .catch((error) => console.log(error));
+
+  return response.data;
 };
 
 const main = async () => {
-  await doRequest();
+  const issues = await getIssues();
+
+  if (Array.isArray(issues)) {
+    for (const issue of issues) {
+      const result = await createIssue({
+        "title": issue.title,
+        "body": issue.body,
+      })
+
+      if (result.success) {
+        // TODO : remove issue
+      }
+    }
+  }
 };
 
 main();
